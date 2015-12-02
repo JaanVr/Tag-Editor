500d88ecccd0233397b5084cba3ab69c26670724
JaanVeersoo <jaan_veersoo@hotmail.com>
2015-12-02 18:21:16 +0200

    Added basic playlist functionality

---
+++ PrjVlcWrapper/UCtrlVLCWrapper.vb
@@ -0,0 +1,9 @@
+﻿Public Class UCtrlVLCWrapper
+    Implements IVLCWrapper
+    Private vlcPlayer As New AxAXVLC.AxVLCPlugin()
+
+    Private Sub play(folderpath As String, filename As String) Implements IVLCWrapper.play
+        vlcPlayer.addTarget("file:///" + folderpath + "\" + filename, Nothing, AXVLC.VLCPlaylistMode.VLCPlayListReplaceAndGo, 0)
+        vlcPlayer.play()
+    End Sub
+End Class
